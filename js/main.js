9b6e716b550045c028448cccaa899381e30aa210
Nahuel Belotti <nahuelbelotti@hotmail.com>
2024-04-13 08:33:33 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -4,16 +4,17 @@ const Administrativo = ["Tarea de administración en empresa multinacional", "Ed
 const Comercial = ["Tarea de ventas para empresa multinacional", "Edad mínima 21 años", "Sueldo promedio 850 usd"];
 const Otros = ["Tarea de serviicos en empresa multinacional", "Edad mínima 25 años", "Sueldo promedio 650 usd"];
     
+function buscador_trabajo() {
+    if (nombreTrabajo == "") {
+        alert("No ingresaste ningun trabajo");
+    } else if (nombreTrabajo == "Administrativo") {
+        console.log(Administrativo);
+    } else if (nombreTrabajo == "Comercial") {
+        console.log(Comercial);
+    } else if (nombreTrabajo == "Otros") {
+        console.log(Otros);
+}}
 
-if (nombreTrabajo == "") {
-    alert("No ingresaste ningun trabajo");
-} else if (nombreTrabajo == "Administrativo") {
-    console.log(Administrativo);
-} else if (nombreTrabajo == "Comercial") {
-    console.log(Comercial);
-} else if (nombreTrabajo == "Otros") {
-    console.log(Otros);
-}
-
+buscador_trabajo()
 
 
